fc28a1f14ecc6fe56dca7fac0d1ea0e5246a75b8
hanif365 <m.a.hanifkhan64@gmail.com>
2023-12-31 00:48:16 +0600

    Update Header's text color and button style

--- src/components/HomePageComponents/Header/Header.jsx
+++ src/components/HomePageComponents/Header/Header.jsx
@@ -5,13 +5,13 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <header className="py-28 2xl:pt-40 bg-[url('/bg_hero.jpg')] bg-no-repeat bg-cover">
+    <header className="py-28 2xl:py-64 bg-[url('/bg_hero.jpg')] bg-no-repeat bg-cover">
       <div className="mx-auto px-5 py-16 md:px-10 md:pt-24 lg:pt-32">
         <div className="mx-auto mb-0 w-full max-w-4xl 2xl:max-w-6xl text-center md:mb-12 lg:mb-16">
-          <h1 className="mb-8 text-4xl 2xl:text-8xl font-bold md:text-7xl">
+          <h1 className="text-white mb-8 text-4xl 2xl:text-8xl font-bold md:text-7xl">
             Tasty Delights, Culinary Wonderland
           </h1>
-          <p className="mx-auto mb-5 max-w-4xl text-sm text-[#383636] sm:text-xl md:mb-6 lg:mb-8">
+          <p className="mx-auto mb-5 max-w-4xl text-sm text-[#e9e2e2] sm:text-xl md:mb-6 lg:mb-8">
             Discover, Savor, and Share
           </p>
           <div className="flex items-stretch justify-center">
